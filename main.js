47a2c2be7ae9f1a6922788e1ed71eb31e2ec4e3d
MALScs <mals9011@gmail.com>
2023-11-27 15:29:50 +0300

    Be03_ex3

--- main.js
+++ main.js
@@ -2,29 +2,29 @@ const express = require("express");
 const path = require("path");
 const app = express();
 const port = 2020;
-
+const router = express.Router();
 app.use(express.static(__dirname));
 
-app.get("/", (req, res) => {
+router.get("/", (req, res) => {
     console.log("user entered the main page")
     console.log("Requested URL:", req.url); // testng
     res.sendFile(path.join(__dirname, "SWE363work.html"));
 });
 
-app.get("/myTable", (req, res) => {
+router.get("/myTable", (req, res) => {
     console.log("user entered myTable page")
     res.sendFile(path.join(__dirname, "TablePage.html"));
 });
 
-app.get("/yourRoutine", (req, res) => {
+router.get("/yourRoutine", (req, res) => {
     console.log("user entered yourRoutine page")
     res.sendFile(path.join(__dirname, "YourRoutine.html"));
 });
-app.get("/contact", (req, res) => {
+router.get("/contact", (req, res) => {
     console.log("user entered contact page")
     res.sendFile(path.join(__dirname, "contact.html"));
 });
-
+app.use("/", router);
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
 });
