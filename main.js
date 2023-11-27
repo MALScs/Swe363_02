752c689ce348baba41ca8c19a1b3123de68a8201
MALScs <mals9011@gmail.com>
2023-11-27 15:48:39 +0300

    Be03_ex4

--- main.js
+++ main.js
@@ -1,30 +1,40 @@
 const express = require("express");
 const path = require("path");
 const app = express();
-const port = 2020;
-const router = express.Router();
+const port = 3030;
 app.use(express.static(__dirname));
-
-router.get("/", (req, res) => {
+app.use(express.urlencoded({ extended: true }));
+app.get("/", (req, res) => {
     console.log("user entered the main page")
     console.log("Requested URL:", req.url); // testng
     res.sendFile(path.join(__dirname, "SWE363work.html"));
 });
 
-router.get("/myTable", (req, res) => {
+app.get("/myTable", (req, res) => {
     console.log("user entered myTable page")
     res.sendFile(path.join(__dirname, "TablePage.html"));
 });
 
-router.get("/yourRoutine", (req, res) => {
+app.get("/yourRoutine", (req, res) => {
     console.log("user entered yourRoutine page")
     res.sendFile(path.join(__dirname, "YourRoutine.html"));
 });
-router.get("/contact", (req, res) => {
+app.get("/contact", (req, res) => {
     console.log("user entered contact page")
     res.sendFile(path.join(__dirname, "contact.html"));
 });
-app.use("/", router);
+app.use(
+    express.urlencoded({
+      extended: true,
+    })
+  );
+  
+  // POST endpoint at /submitOrder
+  app.post("/confirmYourTable.html", (req, res) => {
+    console.log("THE GET METHOD HAS WOPRLED") // TESTING
+    res.sendFile(path.join(__dirname, "confirmYourTable.html"));
+  });
+
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
 });
